74d1f8859e23245b9859840b4b7a9ac2897461cc
AmirAli Fallahi <amirfallahi222@gmail.com>
2021-06-05 15:58:42 +0430

    test main

--- Hospital/Main.java
+++ Hospital/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("salam");
-
         System.out.println("Welcome to Behrooz Online Hospital Service.");
         management.optionsMenu();
 
