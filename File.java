ffc470ff60a2eea9e0c34bc295693cb29eb88e12
AmirAli Fallahi <amirfallahi222@gmail.com>
2021-06-05 18:34:01 +0430

    initializing database first step

--- File.java
+++ File.java
@@ -3,6 +3,40 @@ package Hospital;
 import java.util.ArrayList;
 
 public class File {
+    public static final String DB_NAME = "hospital.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:D:\\databases\\" + DB_NAME;
+
+    public static final String TABLE_DOCTORS = "doctors";
+    public static final String COLUMN_DOCTOR_NAME = "name";
+    public static final String COLUMN_DOCTOR_NATIONALID = "n_id";
+    public static final String COLUMN_DOCTOR_SPECIALTY = "specialty";
+
+    public static final String TABLE_PATIENTS = "patients";
+    public static final String COLUMN_PATIENT_NAME = "name";
+    public static final String COLUMN_PATIENT_NATIONALID = "n_id";
+    public static final String COLUMN_PATIENT_ILLNESS = "illness";
+
+    public static final String TABLE_DRUGS = "drugs";
+    public static final String COLUMN_DRUG_NAME = "name";
+    public static final String COLUMN_DRUG_COMPANY = "company";
+    public static final String COLUMN_DRUG_MAKEDATE = "makeDate";
+    public static final String COLUMN_DRUG_EXPIREDATE = "expireDate";
+
+    public static final String TABLE_MESSAGES = "messages";
+    public static final String COLUMN_MESSAGE_DOCTOR = "doctor";
+    public static final String COLUMN_MESSAGE_PATIENT = "patient";
+    public static final String COLUMN_MESSAGE_SUBJECT = "subject";
+    public static final String COLUMN_MESSAGE_DATE = "date";
+    public static final String COLUMN_MESSAGE_VISITED = "visited";
+
+    public static final String TABLE_VISITS = "visits";
+    public static final String COLUMN_VISIT_ID = "_id";
+    public static final String COLUMN_VISIT_DOCTOR = "doctor";
+    public static final String COLUMN_VISIT_PATIENT = "patient";
+    public static final String COLUMN_VISIT_DRUG = "drug";
+    public static final String COLUMN_VISIT_DATE = "date";
+
+
     private static ArrayList<Doctor> doctors = new ArrayList<>();
     private static ArrayList<Patient> patients = new ArrayList<>();
     private static ArrayList<Drug> drugs = new ArrayList<>();
